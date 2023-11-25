eb180089a0bd43fd0aee518cbde18304ec9de1de
ayoub <ayoub.barich@etu.univ-amu.fr>
2023-11-25 20:46:06 +0100

    implementation of Clause and Literal Classes

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ayoub is a bg");
+        Literal x = new Literal(1);
+        Literal y = new Literal(2);
+        Clause xory = new Clause(new ArrayList<>(List.of(x,y)));
+        System.out.println(xory);
     }
 }
