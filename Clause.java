33e099cb2cc7a48730409458d7999b4824887ee9
ayoub <ayoub.barich@etu.univ-amu.fr>
2023-11-25 20:34:44 +0100

    implementation of Clause and Literal Classes

--- Clause.java
+++ Clause.java
@@ -1,2 +1,16 @@
+import java.util.List;
+
 public class Clause {
+    private List<Literal> literals ;
+
+    public Clause(List<Literal> _literals){
+        literals = _literals;
+    }
+
+    public List<Literal> getLiterals() {
+        return literals;
+    }
+    public void insert(Literal literal){
+        literals.add(literal);
+    }
 }
--- Literal.java
+++ Literal.java
@@ -1,7 +1,36 @@
 
 public class Literal {
-    public int integerValue;
-    public  Boolean truthValue;
-    public String variableName;
+    private int integerValue;
+    private   Boolean truthValue;
+    //private String variableName;
 
+    public Literal(int assignedIntegerValue){
+        integerValue = assignedIntegerValue;
+        truthValue = assignedIntegerValue%2 !=0;
+    }
+
+    public Boolean getTruthValue() {
+        return truthValue;
+    }
+    public int getIntegerValue() {
+        return integerValue;
+    }
+
+/*    public String getVariableName() {
+        return variableName;
+    }*/
+
+    public void setIntegerValue(int integerValue) {
+        this.integerValue = integerValue;
+        this.truthValue = integerValue%2 !=0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return integerValue == ((Literal) obj).getIntegerValue();
+    }
+    @Override
+    public String toString(){
+        return  "<"+integerValue+" , "+ truthValue +" >";
+    }
 }
